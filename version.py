7e3258e7db37a1f3827ecee4773d2dc3aa87b3c5
AndraeCarotta <andrae.carotta@hotmail.com>
2025-08-23 03:14:48 +0200

    AC: moving version.py to highest layer

--- version.py
+++ version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.dev8+g491ac0fc1.d20250822'
-__version_tuple__ = version_tuple = (0, 1, 'dev8', 'g491ac0fc1.d20250822')
+__version__ = version = '0.1.dev11+g4783a186c.d20250823'
+__version_tuple__ = version_tuple = (0, 1, 'dev11', 'g4783a186c.d20250823')
 
-__commit_id__ = commit_id = 'g491ac0fc1'
+__commit_id__ = commit_id = 'g4783a186c'
