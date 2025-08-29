858bf6592098263cfc0d6e76281a109d99a70b12
jonas <jonasb.tech@gmail.com>
2025-08-29 13:52:56 +0000

    jonas: add ares default header

--- ares/utils/decorators.py
+++ ares/utils/decorators.py
@@ -1,13 +1,41 @@
-"""
+r"""
+________________________________________________________________________
+|                                                                      |
+|               $$$$$$\  $$$$$$$\  $$$$$$$$\  $$$$$$\                  |
+|              $$  __$$\ $$  __$$\ $$  _____|$$  __$$\                 |
+|              $$ /  $$ |$$ |  $$ |$$ |      $$ /  \__|                |
+|              $$$$$$$$ |$$$$$$$  |$$$$$\    \$$$$$$\                  |
+|              $$  __$$ |$$  __$$< $$  __|    \____$$\                 |
+|              $$ |  $$ |$$ |  $$ |$$ |      $$\   $$ |                |
+|              $$ |  $$ |$$ |  $$ |$$$$$$$$\ \$$$$$$  |                |
+|              \__|  \__|\__|  \__|\________| \______/                 |
+|                                                                      |
+|              Automated Rapid Embedded Simulation (c)                 |
+|______________________________________________________________________|
+
+# Copyright (c) 2025 Andrä Carotta
+#
+# Licensed under the MIT License. See the LICENSE file for details.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# You may obtain a copy of the License at
+# https://github.com/AndraeCarotta/ares/blob/master/LICENSE
 
 """
-import functools
 
-def safely_run(default_return=None, message:str=None, log_level:str=None, cb=None, log=None):
-    """ provides try/except functionality via decorator
-    
-    Args: 
-        default_return : default return value in case of failure -> depends on function 
+
+def safely_run(
+    default_return=None, message: str = None, log_level: str = None, cb=None, log=None
+):
+    """provides try/except functionality via decorator
+
+    Args:
+        default_return : default return value in case of failure -> depends on function
         message[str] : logger message to display in case of failure
         log_level[str] : log level to use
     """
@@ -69,5 +97,6 @@ def wrap(fcn):
         return res
 
     return wrapped_f
+
 
 return wrap
