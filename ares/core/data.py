2f951b743945a9fba585dc537f2c5d944c65126f
Jonas Bleich <jonas.bleich@gmx.de>
2025-11-01 12:35:35 +0100

    update data.py corresponding new interfaces

--- ares/core/data.py
+++ ares/core/data.py
@@ -28,123 +28,139 @@ ________________________________________________________________________
 
 """
 
-import os
-from typing import Any, Dict, Iterable, Optional
+from typing import Literal
 
 from typeguard import typechecked
 
+from ares.utils.data.ares_interface import AresDataInterface
 from ares.utils.data.mf4_interface import (
     mf4_handler,
 )
-from ares.utils.hash import sha256_filepath
 from ares.utils.logger import create_logger
-from ares.utils.signal import signal
 
 logger = create_logger("data")
 
 
-class Data:
-    @typechecked
-    def __init__(
-        self,
-        file_path: str,
-        base_wf_element_name: str,
-        source: Iterable[str],
-        step_size_init_ms: int,
-    ):
-        """Initializes the Data class by reading a data source file.
+@typechecked
+def get_interface(
+    file_path: str | None = None, 
+    output_format: Literal["mf4","mat"] | None = None
+) -> AresDataInterface:
+    """ Function initializes "ares_interface" depending on the  file-type requested.
+    """
+    if file_path.endswith(".mf4") || output_format == "mf4":
+        return mf4_handler(file_path)
+    elif file_path.endswith(".parquet") || output_format == "parquet":
+        logger.error("Evaluation of .parquet input/output is not implemented yet.")
+        raise ValueError("Not implemented yet.")
+    elif file_path.endswith(".mat") || output_format == "mat":
+        logger.error("Evaluation of .mat input/output is not implemented yet.")
+        raise ValueError("Not implemented yet.")
+    else:
+        logger.error(f"Unknown file format for file: {file_path} or {output_format}")
+        raise ValueError("Unknown file format.")
 
-        The constructor automatically loads and preprocesses the data based on the file
-        extension. The processed data is stored in the `self.data` attribute.
+# class Data:
+#     @typechecked
+#     def __init__(
+#         self,
+#         file_path: str,
+#         base_wf_element_name: str,
+#         source: Iterable[str],
+#         step_size_init_ms: int,
+#     ):
+#         """Initializes the Data class by reading a data source file.
 
-        Args:
-            file_path (str): The path to the data source file (.mf4, .parquet, or .mat).
-            base_wf_element_name (str): The base name of the workflow element associated
-                with this data source.
-            source (Iterable[str]): Specifies which source(s) from the data file to include.
-                Use ['all'] to load all sources, or a list of specific source names (e.g.,
-                ['source1', 'source2']).
-            step_size_init_ms (int): The target resampling step size in milliseconds for
-                the initial loading.
-        """
-        self._file_path = file_path
-        self.hash = sha256_filepath(self._file_path)
-        self.element = base_wf_element_name  # TODO: necessary, check again?
+#         The constructor automatically loads and preprocesses the data based on the file
+#         extension. The processed data is stored in the `self.data` attribute.
 
-        # get fileformat to trigger the correct loading pipeline
-        input_format = os.path.splitext(self._file_path)[1].lower()
-        if input_format == ".mf4":
-            data_handler = mf4_handler(self._file_path)
+#         Args:
+#             file_path (str): The path to the data source file (.mf4, .parquet, or .mat).
+#             base_wf_element_name (str): The base name of the workflow element associated
+#                 with this data source.
+#             source (Iterable[str]): Specifies which source(s) from the data file to include.
+#                 Use ['all'] to load all sources, or a list of specific source names (e.g.,
+#                 ['source1', 'source2']).
+#             step_size_init_ms (int): The target resampling step size in milliseconds for
+#                 the initial loading.
+#         """
+#         self._file_path = file_path
+#         self.element = base_wf_element_name  # TODO: necessary, check again?
 
-        elif input_format == ".parquet":
-            logger.error(
-                "Evaluation of .parquet input/output is not implemented yet",
-            )  # TODO:
-        elif input_format == ".mat":
-            logger.error(
-                "Evaluation of .mat input/output is not implemented yet"
-            )  # TODO:
-        else:
-            logger.error(f"Unknown file format for {self._file_path}.")
+#         # get fileformat to trigger the correct loading pipeline
+#         input_format = os.path.splitext(self._file_path)[1].lower()
+#         if input_format == ".mf4":
+#             data_handler = mf4_handler(self._file_path)
 
-        tmp_source = None if source == ["all"] else source
-        self.data = data_handler.get(tmp_source, stepsize_ms=step_size_init_ms)
+#         elif input_format == ".parquet":
+#             logger.error(
+#                 "Evaluation of .parquet input/output is not implemented yet",
+#             )  # TODO:
+#         elif input_format == ".mat":
+#             logger.error(
+#                 "Evaluation of .mat input/output is not implemented yet"
+#             )  # TODO:
+#         else:
+#             logger.error(f"Unknown file format for {self._file_path}.")
 
-    def _legacy_convert2dict(self):
-        data_dict = {}
-        data_dict["timestamps"] = self.data[0].timestamps
-        [data_dict.update({d.label: d.data}) for d in self.data]
-        return data_dict
+#         tmp_source = None if source == ["all"] else source
+#         self.data = data_handler.get(tmp_source, stepsize_ms=step_size_init_ms)
 
-    def _legacy_convert2list(self, data: dict) -> list[signal]:
-        """Legacy convert function. Converts ARES data dictionalry in to signal list for a better future."""
-        dh = list()
-        timestamps = data["timestamps"]
-        for sig, samples in data.items():
-            dh.append(signal(label=sig, timestamps=timestamps, data=samples))
-        return dh
+#     def _legacy_convert2dict(self):
+#         data_dict = {}
+#         data_dict["timestamps"] = self.data[0].timestamps
+#         [data_dict.update({d.label: d.data}) for d in self.data]
+#         return data_dict
 
-    @typechecked
-    @staticmethod
-    def write_out(
-        file_path: str,
-        output_format: str,
-        data: list[signal],
-        meta_data: Dict[str, Any],
-    ) -> Optional[str]:
-        """Writes data from a specified source within `self.data` to an output file.
+#     def _legacy_convert2list(self, data: dict) -> list[signal]:
+#         """Legacy convert function. Converts ARES data dictionalry in to signal list for a better future."""
+#         dh = list()
+#         timestamps = data["timestamps"]
+#         for sig, samples in data.items():
+#             dh.append(signal(label=sig, timestamps=timestamps, data=samples))
+#         return dh
 
-        The final file path is constructed by combining `dir_path` with a new filename,
-        which includes a timestamps and the specified `output_format`.
-        Currently, only .mf4 output is supported.
+#     @typechecked
+#     @staticmethod
+#     def write_out(
+#         file_path: str,
+#         output_format: str,
+#         data: list[signal],
+#         meta_data: Dict[str, Any],
+#     ) -> Optional[str]:
+#         """Writes data from a specified source within `self.data` to an output file.
 
-        Args:
-            file_path (str): The path to the file that should be created.
-            output_format (str): The desired file extension (e.g., 'mf4').
-            data (list[signal]): A list of signals to be written to the file.
-            meta_data (dict[str, any]): Current ARES and workstation meta data.
+#         The final file path is constructed by combining `dir_path` with a new filename,
+#         which includes a timestamps and the specified `output_format`.
+#         Currently, only .mf4 output is supported.
 
-        Returns:
-            str or None: The full path of the output file, or None if an error occurs.
-        """
-        if output_format == "mf4":
-            data_handler = mf4_handler()
+#         Args:
+#             file_path (str): The path to the file that should be created.
+#             output_format (str): The desired file extension (e.g., 'mf4').
+#             data (list[signal]): A list of signals to be written to the file.
+#             meta_data (dict[str, any]): Current ARES and workstation meta data.
 
-        elif output_format == "parquet":
-            logger.error(
-                "Evaluation of .parquet input/output is not implemented yet",
-            )  # TODO:
-        elif output_format == "mat":
-            logger.error(
-                "Evaluation of .mat input/output is not implemented yet",
-            )  # TODO:
-        else:
-            logger.warning(
-                f"Unsupported data output file format: {output_format}.",
-            )
-            file_path = None
+#         Returns:
+#             str or None: The full path of the output file, or None if an error occurs.
+#         """
+#         if output_format == "mf4":
+#             data_handler = mf4_handler()
 
-        data_handler.write(data)
-        data_handler.save_file(file_path)
+#         elif output_format == "parquet":
+#             logger.error(
+#                 "Evaluation of .parquet input/output is not implemented yet",
+#             )  # TODO:
+#         elif output_format == "mat":
+#             logger.error(
+#                 "Evaluation of .mat input/output is not implemented yet",
+#             )  # TODO:
+#         else:
+#             logger.warning(
+#                 f"Unsupported data output file format: {output_format}.",
+#             )
+#             file_path = None
 
-        return file_path
+#         data_handler.write(data)
+#         data_handler.save_file(file_path)
+
+#         return file_path
---
+++ ares_architecture.md
@@ -0,0 +1,78 @@
+# ARES architecture
+
+## ARES content architecture
+
+```mermaid
+  architecture-beta
+      group ares_base(server)[ares baselayer]
+
+      service data_element(disk)[signal] in ares_base
+      service parameter_element(disk)[parameter] in ares_base
+
+      group ares_interface(server)[ares interfaces]
+
+      service data_interface(server)[data_signals] in ares_interface
+      service parameter_interface(server)[parameter_set] in ares_interface
+
+
+      group ares_plugin(server)[ares plugins]
+
+      service sim_unit(database)[simunit] in ares_plugin
+      service custom_plugins(database)[xxx] in ares_plugin
+```
+
+## ARES classes
+```mermaid
+---
+title: ARES classes
+---
+classDiagram
+    class ares_signal{
+        <<base type>>
+        +String : label
+        +Array[float] : timestamps
+        +Array[Any] : values
+        resample(int: step_size_ms)
+    }
+
+    note for ares_signal "implement missing attributes"
+    class ares_parameter{
+        <<base type>>
+        +String : label
+        +String : dtype
+        +Any | Array[Any] : value
+    }
+
+    class ares_data_interface{
+        <<interface>>
+        +String : filepath
+        -get() -> list[ares_signal]
+        -write_out() -> filepath
+    }
+    class ares_parameter_interface{
+        <<interface>>
+        +String : filepath
+        get() -> list[ares_parameter]
+        write_out() -> filepath
+        change(str: label, any: value)
+    }
+    note for simunit "execute() as default execution function"
+    class simunit{
+        <<plugin>>
+        +ares_data_interface : data_element
+        +ares_parameter_interface : parameter_element
+        execute() -> ares_data_interface
+    }
+    class param_merge{
+        <<plugin>>
+        +ares_parameter_interface : parameter_element
+        +ares_parameter_interface : parameter_element
+        execute() -> ares_parameter_interface
+    }
+    class data_merge{
+        <<plugin>>
+        +ares_data_interface : data_element
+        +ares_data_interface : data_element
+        execute() -> ares_data_interface
+    }
+```
