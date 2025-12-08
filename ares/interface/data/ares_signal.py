24c676400cd0e28e6577aa5074225b513dbfe8ee
Jonas Bleich <jonas.bleich@gmx.de>
2025-12-08 20:23:39 +0100

    get signal and test from jonas branch

--- ares/interface/data/ares_signal.py
+++ ares/interface/data/ares_signal.py
@@ -27,3 +27,44 @@ ________________________________________________________________________
 # https://github.com/AndraeCarotta/ares/blob/master/LICENSE
 
 """
+
+from dataclasses import dataclass, field
+
+import numpy as np
+from numpy.typing import NDArray
+
+
+@dataclass()
+class signal:
+    """A python dataclass to store signals in ARES."""
+
+    label: str
+    timestamps: NDArray[np.floating] = field(
+        default_factory=lambda: np.array([], dtype=np.floating)
+    )
+    data: NDArray[np.object_] = field(
+        default_factory=lambda: np.array([], dtype=np.object_)
+    )
+
+    def __post_init__(self):
+        """
+        Validate the data types after initialization.
+        """
+        if not np.issubdtype(self.timestamps.dtype, np.floating):
+            raise TypeError("The 'timestamps' array must have a float datatype.")
+        if self.timestamps.ndim != 1 or (
+            self.data.ndim >= 1 and self.timestamps.shape[0] != self.data.shape[0]
+        ):
+            raise ValueError(
+                "Both 'timestamps' and 'data' arrays must be at least 1-dimensional."
+            )
+
+    def resample(self, timestamps_resampled: NDArray[np.floating]):
+        """Resample function for ares signals
+        Algortihms available:
+            - linear interpolation (default)
+        """
+        self.data = np.interp(
+            timestamps_resampled, self.timestamps, self.data.astype(np.floating)
+        ).astype(self.data.dtype)
+        self.timestamps = timestamps_resampled
