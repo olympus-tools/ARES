6f9431079e719a759e84d3cf7b50b4cd73a58a0b
AndraeCarotta <andrae.carotta@hotmail.com>
2026-01-08 00:46:33 +0100

    AC: update NOTICE file

--- ares/core/version.py
+++ ares/core/version.py
@@ -12,7 +12,8 @@ __all__ = [
 
 TYPE_CHECKING = False
 if TYPE_CHECKING:
-    from typing import Tuple, Union
+    from typing import Tuple
+    from typing import Union
 
     VERSION_TUPLE = Tuple[Union[int, str], ...]
     COMMIT_ID = Union[str, None]
@@ -27,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = "0.1.dev37+g090429ecf.d20251207"
-__version_tuple__ = version_tuple = (0, 1, "dev37", "g090429ecf.d20251207")
+__version__ = version = '0.1.dev104+g248c32c68.d20260109'
+__version_tuple__ = version_tuple = (0, 1, 'dev104', 'g248c32c68.d20260109')
 
-__commit_id__ = commit_id = "g090429ecf"
+__commit_id__ = commit_id = 'g248c32c68'
