07aae93422cc2190f4b75f57201580ff6d430557
AndraeCarotta <andrae.carotta@hotmail.com>
2026-01-18 13:28:23 +0100

    AC: update docstring header

--- examples/sim_unit/inout_handling/wrapper.c
+++ examples/sim_unit/inout_handling/wrapper.c
@@ -1,34 +1,37 @@
 /*
- * ________________________________________________________________________
- * |                                                                      |
- * |               $$$$$$\  $$$$$$$\  $$$$$$$$\  $$$$$$\                  |
- * |              $$  __$$\ $$  __$$\ $$  _____|$$  __$$\                 |
- * |              $$ /  $$ |$$ |  $$ |$$ |      $$ /  \__|                |
- * |              $$$$$$$$ |$$$$$$$  |$$$$$\    \$$$$$$\                  |
- * |              $$  __$$ |$$  __$$< $$  __|    \____$$\                 |
- * |              $$ |  $$ |$$ |  $$ |$$ |      $$\   $$ |                |
- * |              $$ |  $$ |$$ |  $$ |$$$$$$$$\ \$$$$$$  |                |
- * |              \__|  \__|\__|  \__|\________| \______/                 |
- * |                                                                      |
- * |              Automated Rapid Embedded Simulation (c)                 |
- * |______________________________________________________________________|
- *
- * Copyright 2025 Andrä Carotta
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * For details, see: https://github.com/olympus-tools/ARES#7-license
- */
+________________________________________________________________________
+|                                                                      |
+|               $$$$$$\  $$$$$$$\  $$$$$$$$\  $$$$$$\                  |
+|              $$  __$$\ $$  __$$\ $$  _____|$$  __$$\                 |
+|              $$ /  $$ |$$ |  $$ |$$ |      $$ /  \__|                |
+|              $$$$$$$$ |$$$$$$$  |$$$$$\    \$$$$$$\                  |
+|              $$  __$$ |$$  __$$< $$  __|    \____$$\                 |
+|              $$ |  $$ |$$ |  $$ |$$ |      $$\   $$ |                |
+|              $$ |  $$ |$$ |  $$ |$$$$$$$$\ \$$$$$$  |                |
+|              \__|  \__|\__|  \__|\________| \______/                 |
+|                                                                      |
+|              Automated Rapid Embedded Simulation (c)                 |
+|______________________________________________________________________|
+
+Copyright 2025 olympus-tools contributors. Contributors to this project
+are listed in the NOTICE file:
+
+    https://github.com/olympus-tools/ARES/blob/master/NOTICE
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License:
+
+    https://github.com/olympus-tools/ARES/blob/master/LICENSE
+*/
 
 #include "inout_handling.h"
 #include <stdint.h>
--- packages/param_dcm/param_dcm/param_dcm.py
+++ packages/param_dcm/param_dcm/param_dcm.py
@@ -13,7 +13,10 @@ ________________________________________________________________________
 |              Automated Rapid Embedded Simulation (c)                 |
 |______________________________________________________________________|
 
-Copyright 2025 Andrä Carotta
+Copyright 2025 olympus-tools contributors. Contributors to this project
+are listed in the NOTICE file:
+
+    https://github.com/olympus-tools/ARES/blob/master/NOTICE
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -25,9 +28,9 @@ Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
-limitations under the License.
+limitations under the License:
 
-For details, see: https://github.com/olympus-tools/ARES#7-license
+    https://github.com/olympus-tools/ARES/blob/master/LICENSE
 """
 
 import re
