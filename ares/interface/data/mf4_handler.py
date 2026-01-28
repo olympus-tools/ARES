b2ffa10acf06aea55e63400763fbc1d2910d1e9f
AndraeCarotta <andrae.carotta@hotmail.com>
2026-01-28 22:30:37 +0100

    AC: update/bugfix of data interface classes

--- ares/interface/data/mf4_handler.py
+++ ares/interface/data/mf4_handler.py
@@ -75,6 +75,7 @@ class MF4Handler(MDF, AresDataInterface):
     def __init__(
         self,
         file_path: str | None,
+        vstack_pattern: list[str] | None = None,
         **kwargs,
     ):
         """Initialize MF4Handler and load available channels.
@@ -85,13 +86,19 @@ class MF4Handler(MDF, AresDataInterface):
 
         Args:
             file_path (str | None): Path to the mf4 file to load or write.
+            vstack_pattern (list[str] | None): Pattern (regex) used to stack AresSignal's
             **kwargs (Any): Additional arguments passed to asammdf's MDF constructor.
         """
 
-        AresDataInterface.__init__(self, file_path=file_path, **kwargs)
+        AresDataInterface.__init__(
+            self,
+            file_path=file_path,
+            dependencies=kwargs.pop("dependencies", None),
+            vstack_pattern=vstack_pattern,
+        )
 
         data = kwargs.pop("data", [])
-        if file_path is None or file_path == "":
+        if file_path is None:
             super().__init__(**kwargs)
             self._available_signals: list[str] = []
 
@@ -102,10 +109,6 @@ class MF4Handler(MDF, AresDataInterface):
                 return
 
         else:
-            if not os.path.isfile(file_path):
-                raise FileNotFoundError(
-                    "The signal file requested to read doesn't exist. File requested: {file_path}"
-                )
             super().__init__(file_path, **kwargs)
             self._available_signals = list(self.channels_db.keys())
 
@@ -140,12 +143,12 @@ class MF4Handler(MDF, AresDataInterface):
     @error_msg(
         exception_msg="Error in mf4-handler get function.",
         log=logger,
-        include_args=["label_filter"],
     )
     @typechecked
     def get(
         self,
         label_filter: list[str] | None = None,
+        stepsize: int | None = None,
         vstack_pattern: list[str] | None = None,
         **kwargs,
     ) -> list[AresSignal] | None:
@@ -154,7 +157,8 @@ class MF4Handler(MDF, AresDataInterface):
         Args:
             label_filter (list[str] | None): List of signal names or pattern to read from mf4 file.
                 If None, all available signals are read. Defaults to None.
-            vstack_pattern (list[str]): Pattern (regex) used to stack AresSignal's
+            stepsize (int | None): Step size for resampling signals. If None, no resampling is performed. Defaults to None.
+            vstack_pattern (list[str] | None): Pattern (regex) used to stack AresSignal's
             **kwargs (Any): Additional arguments. 'stepsize' (int) triggers resampling.
 
         Returns:
@@ -164,9 +168,9 @@ class MF4Handler(MDF, AresDataInterface):
         vstack_pattern = (
             self._vstack_pattern
             if vstack_pattern is None
-            else vstack_pattern.append(self._vstack_pattern)
+            else (self._vstack_pattern or []) + vstack_pattern
         )
-        stepsize = kwargs.pop("stepsize", None)
+
         tmp_data = (
             self._get_signals(label_filter=self._available_signals, **kwargs)
             if label_filter is None
@@ -176,20 +180,20 @@ class MF4Handler(MDF, AresDataInterface):
             )
         )
 
-        if vstack_pattern:
-            logger.debug(
-                f"Vertical stacking will be applied considering regex: {self._vstack_pattern}"
-            )
-            tmp_data = self._vstack(data=tmp_data, vstack_pattern=vstack_pattern)
-
         if not tmp_data:
             return None
 
-        if stepsize is None:
-            return tmp_data
-        else:
+        if vstack_pattern:
+            logger.debug(
+                f"Vertical stacking will be applied considering regex: {vstack_pattern}"
+            )
+            tmp_data = self._vstack(data=tmp_data, vstack_pattern=vstack_pattern)
+
+        if stepsize:
             logger.debug(f"Resampling all signals to: {stepsize}")
             return self._resample(data=tmp_data, stepsize=stepsize)
+        else:
+            return tmp_data
 
     def _resolve_label_filter(
         self,
