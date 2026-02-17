42047a83612be54da027a1611a4778546d738fcf
AndraeCarotta <andrae.carotta@hotmail.com>
2026-02-17 22:26:03 +0100

    AC: testcommit

--- ares/core/version.py
+++ ares/core/version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.dev151+g826952d26.d20260121'
-__version_tuple__ = version_tuple = (0, 1, 'dev151', 'g826952d26.d20260121')
+__version__ = version = '0.1.dev187+ge24654a87.d20260217'
+__version_tuple__ = version_tuple = (0, 1, 'dev187', 'ge24654a87.d20260217')
 
-__commit_id__ = commit_id = 'g826952d26'
+__commit_id__ = commit_id = 'ge24654a87'
