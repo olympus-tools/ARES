963805b23def48e403fe9c2643799efc1dcf94e7
AndraeCarotta <andrae.carotta@hotmail.com>
2026-02-27 23:38:48 +0100

    AC: merge impelemntation

---
+++ ares/plugins/merge.py
@@ -0,0 +1,54 @@
+r"""
+________________________________________________________________________
+|                                                                      |
+|               $$$$$$\  $$$$$$$\  $$$$$$$$\  $$$$$$\                  |
+|              $$  __$$\ $$  __$$\ $$  _____|$$  __$$\                 |
+|              $$ /  $$ |$$ |  $$ |$$ |      $$ /  \__|                |
+|              $$$$$$$$ |$$$$$$$  |$$$$$\    \$$$$$$\                  |
+|              $$  __$$ |$$  __$$< $$  __|    \____$$\                 |
+|              $$ |  $$ |$$ |  $$ |$$ |      $$\   $$ |                |
+|              $$ |  $$ |$$ |  $$ |$$$$$$$$\ \$$$$$$  |                |
+|              \__|  \__|\__|  \__|\________| \______/                 |
+|                                                                      |
+|              Automated Rapid Embedded Simulation (c)                 |
+|______________________________________________________________________|
+
+Copyright 2025 olympus-tools contributors. Dependencies and licenses
+are listed in the NOTICE file:
+
+    https://github.com/olympus-tools/ARES/blob/master/NOTICE
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License:
+
+    https://github.com/olympus-tools/ARES/blob/master/LICENSE
+"""
+
+from ares.interface.data.ares_data_interface import AresDataInterface
+from ares.interface.parameter.ares_parameter_interface import AresParamInterface
+
+
+def ares_plugin(plugin_input):
+    """ARES plugin that merges multiple ARES outputs into a single output.
+
+    Args:
+        plugin_input (dict): A dictionary containing the following keys:
+            - "inputs" (list): A list of input file paths to be merged.
+            - "output" (str): The output file path for the merged result.
+
+    Returns:
+        None
+    """
+
+    element_lists: list[list[AresParamInterface]] | list[list[AresDataInterface]] = (
+        plugin_input.get("input", None)
+    )
