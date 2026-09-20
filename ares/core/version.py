c6517dfcee5789c7f8965bdc4cec7c1e065de2bb
AndraeCarotta <andrae.carotta@hotmail.com>
2026-09-21 00:48:06 +0200

    AC: update dcmi submodule

--- ares/core/version.py
+++ ares/core/version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.1.dev239+gc8bdea14c.d20260917'
-__version_tuple__ = version_tuple = (0, 1, 'dev239', 'gc8bdea14c.d20260917')
+__version__ = version = '0.1.dev241+g17f102f75.d20260920'
+__version_tuple__ = version_tuple = (0, 1, 'dev241', 'g17f102f75.d20260920')
 
-__commit_id__ = commit_id = 'gc8bdea14c'
+__commit_id__ = commit_id = 'g17f102f75'
